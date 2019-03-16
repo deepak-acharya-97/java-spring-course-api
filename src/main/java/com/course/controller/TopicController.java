228621f03cbe6d4fe5f471348396561e7e15915d
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-03-16 22:24:36 +0530

    Update Controller

--- src/main/java/com/course/controller/TopicController.java
+++ src/main/java/com/course/controller/TopicController.java
@@ -22,4 +22,14 @@ public class TopicController {
 	public Topic getTopic(@PathVariable String id) {
 		return topicService.getTopic(id);
 	}
+	
+	@RequestMapping(value="/topics", method=RequestMethod.POST)
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	
+	@RequestMapping(value="/topics/{id}", method=RequestMethod.DELETE)
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
+	}
 }
