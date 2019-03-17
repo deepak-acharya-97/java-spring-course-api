3de6aef8f2736be11856339ca44438892265fab0
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-03-17 14:27:11 +0530

    Update Service Layer as per Repository Layer

--- src/main/java/com/course/service/TopicService.java
+++ src/main/java/com/course/service/TopicService.java
@@ -2,45 +2,36 @@ package com.course.service;
 
 import java.util.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.course.model.Topic;
-import com.course.utility.Utilities.Utility;
+import com.course.repository.TopicRepository;
 
 @Service
 public class TopicService {
-	private List<Topic> topics = new ArrayList<>(Arrays.asList(
-			new Topic("987","Data Structures","Trees"),
-			new Topic("899","UNIX Programming","vi commands"),
-			new Topic("677","Operating Systems","pagination")
-	));
-	
+	@Autowired
+	private TopicRepository topicRepository;
 	public List<Topic> getAllTopics(){
+		List<Topic> topics = new ArrayList<Topic>();
+		topicRepository.findAll().forEach(topics::add);
 		return topics;
 	}
 	
 	public Topic getTopic(String id) {
-		Topic topic = topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
-		if(Utility.isNotNull(topic)) return topic;
-		else return new Topic(id,"No Topic Name Found","No Desciption Found");
+		return topicRepository.findById(id).orElse(new Topic(id,"Not Found","Not Found"));
 	}
 	
 	public void addTopic(Topic topic) {
-		topics.add(topic);
+		topicRepository.save(topic);
 	}
 	
 	public void deleteTopic(String id) {
-		topics.removeIf(t -> t.getId().equals(id));
+		topicRepository.deleteById(id);
 	}
 	
 	public void updateTopic(String id, Topic topic) {
-		Topic currentTopic = topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
-		if(Utility.isNotNull(currentTopic)) {
-			String updatedName = Utility.isNotNull(topic.getName()) ? topic.getName() : currentTopic.getName();
-			String updatedDescription = Utility.isNotNull(topic.getDescription()) ? topic.getDescription() : currentTopic.getDescription();
-			currentTopic.setName(updatedName);
-			currentTopic.setDescription(updatedDescription);
-		}
+		topicRepository.save(topic);
 	}
 	
 }
